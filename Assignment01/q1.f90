be6aaafdccfdeba639725dc3ca742055c2209e5c
Justin Hood <juhood@mines.edu>
2017-02-14 08:05:02 -0700

    In class 2/14

--- Assignment01/q1.f90
+++ Assignment01/q1.f90
@@ -40,25 +40,25 @@ program question1
 contains
 	integer function fact(a)
 		implicit none
-		integer, intent(IN) :: a
-		integer p
+		integer, intent(IN) :: alpha
+		integer p, s
 		p=1
 		
-		if(a .eq. 0) then
+		if(alpha .eq. 0) then
 			fact=1
-		else if(a .LE. 0) then
+		else if(alpha .LE. 0) then
 			print*, "This is not an allowed value for factorial"
 			stop
 		else
-			do i=1,a
-				p=p*i
+			do s=1,alpha
+				p=p*s
 			enddo
 			fact=p
 		endif
 
 	end function fact
 	
-	integer function parens(a,b)
+	integer function parens(alpha,b)
 		implicit none
 		integer, intent(IN) :: a, b
 		parens = fact(a)/(fact(b)*fact(a-b))
@@ -70,10 +70,10 @@ contains
 		curly=dble(sin(real(a))/(cos(real(b))*tan(real(a+b))))
 	end function curly
 
-	double precision function bij(i,j,N)
+	double precision function bij(a,b,N)
 		implicit none
-		integer, intent(IN) :: i, j, N
-		bij=((-1)**(i+j))*(i+j-1)*parens(N+i-1,N-j)*parens(N+j-1,N-i)*((parens(i+j-2,i-1))**2)
+		integer, intent(IN) :: a, b, N
+		bij=((-1)**(a+b))*(a+b-1)*parens(N+a-1,N-b)*parens(N+b-1,N-b)*((parens(a+b-2,a-1))**2)
 	end function bij
 
 	double precision function complement(i,j,N)
